64ccf561cf0179ff7311d77c3f83dbb01d4e3477
Student <student@epicodus-2.hsd1.or.comcast.net>
2022-11-10 11:11:21 -0800

    test takeDamage and giveDamage methods, test passed for both
    co-authored-by: Bodie Wood <bodiewood@hotmail.com>

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -10,14 +10,32 @@ describe('Character', () => {
 
   test('this should return 75 hP and 90 strength from the wizard prototype', () => {
     newCharacter.Wizard();
-    expect(newCharacter.hP).toEqual(75);
+    expect(newCharacter.hp).toEqual(75);
     expect(newCharacter.strength).toEqual(90);
   });
 
-  test('this should return 50 hP and 80 strength from the archer prototype', () => {
+  test('this should return 50 hp and 80 strength from the archer prototype', () => {
     newCharacter.Archer();
-    expect(newCharacter.hP).toEqual(50);
+    expect(newCharacter.hp).toEqual(50);
     expect(newCharacter.strength).toEqual(80);
   });
+
+  test('this should return 50 hp and 80 strength from the spy prototype', () => {
+    newCharacter.spy();
+    expect(newCharacter.hp).toEqual(100);
+    expect(newCharacter.strength).toEqual(50);
+  });
+
+  test('this should return 65 to hp', () => {
+    newCharacter.Wizard();
+    newCharacter.takeDamage();
+    expect(newCharacter.hp).toEqual(65);
+  });
+
+  test('this should return 80 to hp', () => {
+    newCharacter.Wizard();
+    newCharacter.giveDamage();
+    expect(newCharacter.hp).toEqual(80);
+});
 
 });
